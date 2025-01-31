191c54dbdff4d456b92aabea84b68a234b7bf6fe
wingchichan <w.chi.chan@gmail.com>
2025-01-31 10:39:20 +0000

    Deployed on Render and udated API url

--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,10 @@
 // js file for our html/client side so data is visible to end user
 const app = document.getElementById("app");
+const API_URL = "https://week4-project-fullstackapp-server.onrender.com";
 
 // making a fetch API request to our server
 async function getReviews() {
-  const response = await fetch("http://localhost:8080/reviews");
+  const response = await fetch(`${API_URL}/reviews`);
   // reads the data in json
   const data = await response.json();
 
